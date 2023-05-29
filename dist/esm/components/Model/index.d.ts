f7a6c61f2193a12f0b9452569aa73c2c6ec30f3a
SamNi <1205470772@qq.com>
2023-05-29 08:55:14 +0800

    modify dist

--- dist/esm/components/Model/index.d.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-interface PopupProps {
-    className?: string;
-    onClick?: (e?: any) => void;
-}
-declare function Model<T extends PopupProps>(props: React.PropsWithChildren<T>): React.ReactElement;
-
-export { Model };
--- dist/esm/components/TUIMessage/MessageContext.d.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Message } from 'tim-js-sdk';
-
-interface MessageContextProps {
-    message?: Message;
-}
-declare function MessageContext(props: MessageContextProps): React.ReactElement;
-
-export { MessageContext, MessageContextProps };
